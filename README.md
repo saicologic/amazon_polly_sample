fe9d99298c0a98473b879a33002e1fdc585cceb0
saicologic <saicologic@gmail.com>
2022-08-11 17:33:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Python Env
+
+```
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+```
+
+# 環境変数の設定
+
+```
+export AWS_PROFILE=xxxx
+```
+
+# 実行
+
+読み上げたい文書を設定する。日本語に固定している
+
+例: Hello World
+
+```
+python ./voice.py say "こんにちは"
+```
---
+++ voice.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import subprocess
+import fire
+
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+from tempfile import gettempdir
+
+class Polly:
+    def __init__(self):
+        pass
+
+    def synthesize_speech(self, text):
+        engine = 'neural'
+
+        try:
+            session = Session(profile_name=os.environ.get('AWS_PROFILE'))
+        except Exception as error:
+            print(error)
+            sys.exit(-1)
+
+        polly = session.client('polly')
+
+        try:
+            response = polly.synthesize_speech(
+                Engine=engine,
+                LanguageCode="ja-JP",
+                Text=text,
+                TextType="text",
+                OutputFormat="mp3",
+                VoiceId="Takumi"
+            )
+        except (BotoCoreError, ClientError) as error:
+            print(error)
+            sys.exit(-1)
+        return response
+
+    def save_audio(self, response):
+        output_path = "./output"
+        output_file = "output.mp3"
+        file = output_path + "/" + output_file
+
+        if os.path.isfile(output_file):
+            os.remove (output_file)
+
+        if "AudioStream" in response:
+            with closing(response["AudioStream"]) as stream:
+                output = os.path.join(output_path, output_file)
+
+                try:
+                    with open(output, "wb") as file:
+                        file.write(stream.read())
+                except IOError as error:
+                    print(error)
+                    sys.exit(-1)
+
+        else:
+            print("Could not stream audio")
+            sys.exit(-1)
+        return output
+
+    def play_audio(self, output):
+        subprocess.call(["open", output])
+
+def say(text):
+    polly = Polly()
+    response = polly.synthesize_speech(text)
+    output = polly.save_audio(response)
+    polly.play_audio(output)
+
+if __name__ == "__main__":
+    fire.Fire()
